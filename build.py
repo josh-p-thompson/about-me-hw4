f483843098b96f39bd89221e0de806b6e741110b
Josh Thompson <jpthompson93@gmail.com>
2019-11-14 21:39:11 -0800

    added navigation bar template

--- build.py
+++ build.py
@@ -2,33 +2,36 @@ import glob
 import os
 from jinja2 import Template
 
-def generate_page(file_name, source, destination, title, index_btn="", projects_btn="", blog_btn=""): 
-    """ generate pages from content and template and write to destination directory """
-    template = Template(open('templates/base.html').read())
-    template.render(
-        title=title,
-        right_block_content=open(source + file_name).read(), 
-        index_btn=index_btn, 
-        projects_btn=projects_btn, 
-        blog_btn=blog_btn,
-    )
-
 def generate_page_list(): 
     """ generate list of pages to be created based on presence in content directory """
     pages = []
     for file in glob.glob("content/*.html"): 
         file_name = os.path.basename(file)
         title, extension = os.path.splitext(file_name)
+        if title == "index": 
+            title = "About"
         pages.append(
             {
                 'file_name': file_name, 
-                'source_directory': 'content/', 
-                'destination_directory': 'docs/', 
+                'source': 'content/', 
+                'destination': 'docs/', 
                 'title': title.capitalize(), 
                 title + '_btn': 'active',
             }
         )
     return pages
+
+def generate_page(file_name, source, destination, title, pages): 
+    """ generate pages from content and template and write to destination directory """
+    template = Template(open('templates/base.html').read())
+    full_page = template.render(
+        title=title,
+        file_name = file_name,
+        destination=destination+file_name,
+        right_block_content=open(source + file_name).read(), 
+        pages=pages,
+    )
+    open(destination + file_name, 'w+').write(full_page)
 
 def main(): 
     pages = generate_page_list()
@@ -36,12 +39,10 @@ def main():
         print(f'generating page {page}\n') 
         generate_page(
             file_name=page['file_name'],
-            source=page['source_directory'], 
-            destination=page['destination_directory'], 
+            source=page['source'], 
+            destination=page['destination'], 
             title=page['title'], 
-            index_btn=page.get('index_btn'), 
-            projects_btn=page.get('projects_btn'), 
-            blog_btn=page.get('blog_btn'),
+            pages=pages,
         )
 
 if __name__ == "__main__": 
