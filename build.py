98419217073f36e0804dbb6cb810a802e415f3e6
Josh Thompson <jpthompson93@gmail.com>
2019-11-14 17:54:21 -0800

    added generate_page_list()

--- build.py
+++ build.py
@@ -1,12 +1,14 @@
 from string import Template
+import glob
+import os
 
-def generate_page(file_name, source, destination, title, about_btn="", projects_btn="", blog_btn=""): 
+def generate_page(file_name, source, destination, title, index_btn="", projects_btn="", blog_btn=""): 
     """ generate pages from content and template and write to destination directory """
     template = Template(import_content('templates/base.html'))
     full_page = template.safe_substitute(
         title=title,
         right_block_content=import_content(source + file_name), 
-        about_btn=about_btn, 
+        index_btn=index_btn, 
         projects_btn=projects_btn, 
         blog_btn=blog_btn,
     )
@@ -16,31 +18,25 @@ def import_content(file_path):
     """ import content files """
     return open(file_path).read()
 
-def main(): 
-    pages = [
-        {
-            'file_name': 'index.html',
-            'source_directory': 'content/',
-            'destination_directory': 'docs/',
-            'title': 'About', 
-            'about_btn': 'active', 
-        }, 
-        {
-            'file_name': 'projects.html',
-            'source_directory': 'content/',
-            'destination_directory': 'docs/',
-            'title': 'Projects', 
-            'projects_btn': 'active', 
-        }, 
-        {
-            'file_name': 'blog.html',
-            'source_directory': 'content/',
-            'destination_directory': 'docs/',
-            'title': 'Blog', 
-            'blog_btn': 'active',
-        }, 
-    ]
+def generate_page_list(): 
+    """ generate list of pages to be created based on presence in content directory """
+    pages = []
+    for file in glob.glob("content/*.html"): 
+        file_name = os.path.basename(file)
+        title, extension = os.path.splitext(file_name)
+        pages.append(
+            {
+                'file_name': file_name, 
+                'source_directory': 'content/', 
+                'destination_directory': 'docs/', 
+                'title': title.capitalize(), 
+                title + '_btn': 'active',
+            }
+        )
+    return pages
 
+def main(): 
+    pages = generate_page_list()
     for page in pages:
         print(f'generating page {page}\n') 
         generate_page(
@@ -48,7 +44,7 @@ def main():
             source=page['source_directory'], 
             destination=page['destination_directory'], 
             title=page['title'], 
-            about_btn=page.get('about_btn'), 
+            index_btn=page.get('index_btn'), 
             projects_btn=page.get('projects_btn'), 
             blog_btn=page.get('blog_btn'),
         )
