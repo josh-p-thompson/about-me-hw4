fccdb2a82ecd8351b6525ea337dec23dd6d79fcf
Josh Thompson <jpthompson93@gmail.com>
2019-11-17 12:53:59 -0800

    last commit before I mess everything up

--- build.py
+++ build.py
@@ -3,7 +3,9 @@ import os
 from jinja2 import Template
 
 def generate_page_list(): 
-    """ generate list of pages to be created based on presence in content directory """
+    """ 
+    generates page list for all html files present in content directory
+    """
     pages = []
     for file in glob.glob("content/*.html"): 
         file_name = os.path.basename(file)
@@ -20,8 +22,8 @@ def generate_page_list():
         )
     return sorted(pages, key = lambda page: page['title']) 
 
-def generate_pages(pages): 
-    """ generates pages from content/template and writes to destination directory """
+def build_pages(pages): 
+    """ generates page from content/template and writes to destination directory """
     template = Template(open('templates/base.html').read())
     for page in pages: 
         print(f'generating page: {page}\n') 
@@ -35,7 +37,7 @@ def generate_pages(pages):
 
 def main(): 
     pages = generate_page_list()
-    generate_pages(pages)
+    build_pages(pages)
 
 if __name__ == "__main__": 
     main()
